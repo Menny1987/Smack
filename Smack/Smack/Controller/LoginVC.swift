015e48ce7cbe2db4c33181612fde09b3ffd2459f
Menny1987 <Mennyyyy@gmail.com>
2017-10-28 17:33:35 +0300

    setting up Login VC

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Menny Atia on 28/10/2017.
+//  Copyright © 2017 Menny Atia. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Menny Atia on 28/10/2017.
+//  Copyright © 2017 Menny Atia. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
