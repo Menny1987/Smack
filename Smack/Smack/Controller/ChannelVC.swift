e76ad65a7e26301514af5aadff0e247d1f0e6ee4
Menny1987 <Mennyyyy@gmail.com>
2017-11-09 00:38:39 +0200

    add modalProfile

--- Smack/Smack/Controller/ChannelVC.swift
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -24,6 +24,16 @@ class ChannelVC: UIViewController {
     }
 
     @IBAction func loginBtnPressed(_ sender: Any) {
+        if AuthService.instance.isLoggedIn {
+        
+            let profile = ProfileVC()
+            profile.modalPresentationStyle = .custom
+            present(profile, animated: true, completion: nil)
+            
+        } else {
+            performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        }
+        
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
     }
     
