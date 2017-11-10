858828663e1b75dec7d83644abb88f86a438220b
Menny1987 <Mennyyyy@gmail.com>
2017-11-10 17:19:27 +0200

    setUp user Login

--- Smack/Smack/Controller/LoginVC.swift
+++ Smack/Smack/Controller/LoginVC.swift
@@ -9,13 +9,44 @@
 import UIKit
 
 class LoginVC: UIViewController {
-
+    
+    //Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpView()
         self.hideKeyboardWhenTappedAround()
+        
       
     }
 
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        
+        guard let email = usernameTxt.text , usernameTxt.text != "" else {return}
+        guard let pass = passwordTxt.text , passwordTxt.text != "" else {return}
+        
+        AuthService.instance.loginUser(email: email, password: pass) { (success) in
+            if success {
+                AuthService.instance.findUserByEmail(completion: { (success) in
+                    if success {
+                        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+                        self.spinner.isHidden = true
+                        self.spinner.stopAnimating()
+                        self.dismiss(animated: true, completion: nil)
+                        
+                    }
+                })
+            }
+        }
+    }
+    
     @IBAction func closePressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
@@ -24,7 +55,21 @@ class LoginVC: UIViewController {
         performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
     }
     
+    
+    func setUpView() {
+        spinner.isHidden = true
+        usernameTxt.attributedPlaceholder = NSAttributedString(string: "שם משתמש", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+        
+        passwordTxt.attributedPlaceholder = NSAttributedString(string: "סיסמא", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+    }
+        
+    
 }
+
+
+        
+        
+    
 
 
 extension UIViewController {
